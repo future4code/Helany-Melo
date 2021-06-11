4337037cc5af338592eb451d7f775e5911398e7e
unknown <helany.f.melo@gmail.com>
2021-06-10 23:11:51 -0300

    adicionando códigos de loops

---
+++ quinzena2/loops/index.js
@@ -0,0 +1,60 @@
+// let pets= Number(prompt("Quantos pets você tem?"))
+// let petArr = []
+// let newPets = ""
+
+// if (pets === 0){
+// 	console.log("Que pena! você pode adotar um pet!")
+
+// }else{
+// 	for(let i=0; i < pets; i++){
+// 		newPets = prompt("Qual o nome do pet?")
+// 		petArr.push(newPets)
+// 	}
+// }
+
+// console.log(petArr)
+
+//
+//const arrayOriginal = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10] 
+
+// function percorrerArray (array){
+// 	for (let i = 0; i < array.length; i++){
+// 		console.log(array[i])
+// 	}
+// }
+
+// percorrerArray(arrayOriginal)
+
+//const arrayOriginal = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10] 
+
+// function percorrerArray (array){
+// 	for (let i = 0; i < array.length; i++){
+// 		console.log(array[i]/10)
+// 	}
+// }
+
+// percorrerArray(arrayOriginal)
+
+
+// let arrayOriginal = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 3] 
+// let numPares =[]
+
+
+// for(let i =0; i < arrayOriginal.length; i++){
+// 	if(arrayOriginal[i] % 2 === 0){
+// 		numPares.push(arrayOriginal[i])
+// 	}
+// }
+
+// console.log(numPares)
+
+let arrayOriginal = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 3] 
+
+// function index (arrayNumOriginal) {
+
+//   for (let i = 0; i < arrayOriginal.length; i++) {
+//       console.log("O elemento do índex " + i + " é " + arrayNumOriginal[i])
+//   }
+// }
+
+// index (arrayOriginal)
