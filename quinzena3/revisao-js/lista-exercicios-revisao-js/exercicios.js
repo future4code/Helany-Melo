820d338101a9ca1b6d810f50bc509180f69b6967
unknown <helany.f.melo@gmail.com>
2021-06-20 13:55:48 -0300

    adicionando ex.6

--- quinzena3/revisao-js/lista-exercicios-revisao-js/exercicios.js
+++ quinzena3/revisao-js/lista-exercicios-revisao-js/exercicios.js
@@ -48,6 +48,20 @@ function retornaQuantidadeElementos(array) {
 
 // EXERCÍCIO 06
 function retornaExpressoesBooleanas() {
+  const booleano1 = true
+  const booleano2 = false
+  const booleano3 = !booleano2 
+  const booleano4 = !booleano3 
+ 
+   const a = booleano1 && booleano2 && !booleano4
+   const b = (booleano1 && booleano2) || !booleano3
+   const c = (booleano2 || booleano3) && (booleano4 || booleano1) 
+   const d = !(booleano2 && booleano3) || !(booleano1 && booleano3) 
+   const e  = !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)
+ 
+   const resposta = [a, b, c, d, e]
+ 
+   return resposta
 
 }
 
