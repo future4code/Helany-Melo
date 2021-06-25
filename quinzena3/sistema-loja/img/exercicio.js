a3a421add72693a09f70e9ab90730ec28beb32f7
unknown <helany.f.melo@gmail.com>
2021-06-24 21:39:15 -0300

    adicionando exercício de js

--- quinzena3/sistema-loja/img/exercicio.js
+++ quinzena3/sistema-loja/img/exercicio.js
@@ -9,3 +9,12 @@ salarioFixo = 2000
         return salarioFixo
     }
 }*/
+
+
+```function calculaPrecoTotal(quantidade){
+    if(quantidade>=12){
+      return quantidade*1.00
+    }else{
+      return quantidade*1.30
+    }
+  }```
